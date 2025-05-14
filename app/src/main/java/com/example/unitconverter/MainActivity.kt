7504c17af9868ca0d859204053a9eb0f8cfdc331
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-14 20:52:01 +0900

    Box, Icons and Dropdown Menus

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -46,15 +48,29 @@ fun UnitConverter(modifier: Modifier = Modifier) {
             },
         )
         Row {
-            val context = LocalContext.current
-            // Here all the UI elements  will be stacked next to each other
-            Button(
-                onClick = {
-                    Toast.makeText(context, "Thanks for clicking!", Toast.LENGTH_SHORT).show()
+            Box {
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = "Select")
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down"
+                    )
                 }
-            ) {
-                Text(text = "Click Me!")
             }
+            Box {
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = "Select")
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down"
+                    )
+                }
+            }
+            // Here all the UI elements  will be stacked next to each other
         }
         Text(text = "Result:")
     }
